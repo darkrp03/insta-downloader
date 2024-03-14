ceb376ed620f9324b727811de77787df7f529f61
unknown <andrejveselyj90@gmail.com>
2024-03-14 04:43:10 +0200

    change getVideoUrl() logic

--- src/instagram/instagram.service.ts
+++ src/instagram/instagram.service.ts
@@ -1,22 +1,43 @@
 import axios from "axios";
 import { InstaCredentials } from "./credentials";
+import { FileApi } from "../file/fileApi";
 
 export class InstagramService {
     async getVideoUrl(postId: string): Promise<string | undefined> {
         const instagramUrl = `https://www.instagram.com/p/${postId}/?__a=1&__d=dis`;
         const headers = InstaCredentials.getHeaders();
-    
+
         const response = await axios.get(instagramUrl, {
             headers: headers
         });
 
         const items = response.data["items"];
         const item = items[0];
-        const video_versions = item["video_versions"];
-        const video_version = video_versions[video_versions.length - 1];
-        const url = video_version["url"];
+        const videoVersions = item["video_versions"];
 
-        return url;
+        if (!videoVersions) {
+            return;
+        }
+
+        const promises: Promise<number>[] = [];
+
+        for (const videoVersion of videoVersions) {
+            const url = videoVersion["url"];
+            promises.push(FileApi.getFileSizeFromUrl(url));
+        }
+
+        const videoSizes = await Promise.all(promises);
+        const maxFileSize = 50;
+
+        for (const index in videoSizes) {
+            const videoSize = videoSizes[index];
+
+            const megabytes = FileApi.convertToMegabytes(videoSize);
+
+            if (megabytes < maxFileSize) {
+                return videoVersions[index]["url"];
+            }
+        }
     }
 
     getPostId(url: string) {
